ca15075521837113bab98f5b7f2920a18b11b83b
serinchu <serinee95@gmail.com>
2018-07-25 03:08:33 +0900

    second commit

--- swapbyte.cpp
+++ swapbyte.cpp
@@ -6,7 +6,5 @@ uint16_t ntoh_2(uint16_t src) {
 }
 
 uint32_t ntoh_4(uint32_t src) {
-	uint16_t front = (src >> 16);
-	uint16_t end = (uint16_t)src;
-	return ((ntoh_2(end) << 16) | ((uint32_t)ntoh_2(front)));
+	return ((ntoh_2((uint16_t)src) << 16) | ((uint32_t)ntoh_2((uint16_t)(src >> 16))));
 }
